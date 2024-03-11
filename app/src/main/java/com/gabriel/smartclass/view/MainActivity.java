1634d3fdb802307699b6303acf55b91a4ab8d563
Gabriel <gabrielferreirasilva04@outlook.com>
2024-03-10 23:53:07 -0300

    primeiro commit

---
+++ app/src/main/java/com/gabriel/smartclass/view/MainActivity.java
@@ -0,0 +1,20 @@
+package com.gabriel.smartclass.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gabriel.smartclass.R;
+import com.google.firebase.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
