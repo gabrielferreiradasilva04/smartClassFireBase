cb6ddfb97c16e85f3e4e660696054ee35cf10f6e
gabriel_silva <gabrielferreirasilva04@outlook.com>
2024-03-15 00:50:12 -0300

    15 03 2024

--- app/src/main/java/com/gabriel/smartclass/model/Course.java
+++ app/src/main/java/com/gabriel/smartclass/model/Course.java
@@ -1,2 +1,8 @@
-package com.gabriel.smartclass.model;public class Course {
+package com.gabriel.smartclass.model;
+
+public class Course {
+    private String documentId;
+    private String title;
+    private String description;
+    private String duration;
 }
